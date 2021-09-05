e66d1edc292c221971431ce23e5c13ffe40fda6b
Haivex <demozial@gmail.com>
2021-09-05 16:40:18 +0200

    nested routing explanation

---
+++ pages/page1/profile.tsx
@@ -0,0 +1,9 @@
+//this is nested route https://localhost:3000/page1/profile
+
+import styles from './styles.module.scss';
+
+export default function Page1() {
+    return (
+            <h2 className={styles.red}>Page 1 profile</h2>
+    )
+}
