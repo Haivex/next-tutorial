3cd52d6cdff11ceb25c52ffe6877e675ce5c47dd
Haivex <demozial@gmail.com>
2021-09-05 16:14:36 +0200

    res, req objects

--- pages/api/random-number.ts
+++ pages/api/random-number.ts
@@ -1,6 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default function randomNumber(req: NextApiRequest, res: NextApiResponse) {
+    //req.body
+    //req.query
+    //res.setCookie res.statusCode
     res.json({status: 'ok', value: Math.random()})
 }
 
