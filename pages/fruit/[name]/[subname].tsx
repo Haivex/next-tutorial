0a5faf2496991864e60c68711bcb04a70c5ac58b
Haivex <demozial@gmail.com>
2021-09-05 16:53:25 +0200

    dynamic routing

---
+++ pages/fruit/[name]/[subname].tsx
@@ -0,0 +1,9 @@
+//https://localhost:3000/fruit/apple WORKS
+//https://localhost:3000/fruit/banana WORKS
+//https://localhost:3000/fruit/apple/red NOT WORKING (we must create folder with  square bracket [] and file with square bracket inside and index file to working)
+
+export default function FruitName() {
+    return (
+            <h2>Fruit 2nd segment</h2>
+    )
+}
