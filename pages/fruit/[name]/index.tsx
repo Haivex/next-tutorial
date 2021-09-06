f1e44848847a1489784887d82797d757d7cf68d4
Haivex <demozial@gmail.com>
2021-09-06 14:12:49 +0200

    router

--- pages/fruit/[name]/index.tsx
+++ pages/fruit/[name]/index.tsx
@@ -2,8 +2,17 @@
 //https://localhost:3000/fruit/banana WORKS
 //https://localhost:3000/fruit/apple/red NOT WORKING (we must create folder with  square bracket [] and file with square bracket inside and index file to working)
 
+import { useRouter } from 'next/dist/client/router'
+
 export default function FruitName() {
+    const router = useRouter();
+
+    function takeHome() {
+        //router.replace('')
+        router.push('/')
+    }
+
     return (
-            <h2>Fruit Index</h2>
+            <h2>Hello {router.query.name} {router.query.subname} <button onClick={takeHome}>Home</button></h2>
     )
 }
